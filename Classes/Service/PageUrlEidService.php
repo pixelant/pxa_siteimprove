8f2a0eb4bdf362376f27ac0d40617009d2f05ec2
anjeylink <andriy.oprysko@resultify.se>
2018-08-23 11:30:35 +0300

    [FEATURE] use EDI to generate url

---
+++ Classes/Service/PageUrlEidService.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Pixelant\PxaSiteimprove\Service;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+use TYPO3\CMS\Frontend\Page\PageGenerator;
+use TYPO3\CMS\Frontend\Utility\EidUtility;
+
+/**
+ * Class PageUrlEidService
+ * @package Pixelant\PxaSiteimprove\Service
+ */
+class PageUrlEidService
+{
+    /**
+     * @var int
+     */
+    protected $pageId = 0;
+
+    /**
+     * Initializes variables for link
+     */
+    public function __construct()
+    {
+        $params = json_decode(base64_decode(GeneralUtility::_GP('data')), true);
+
+        if (is_array($params)) {
+            $this->pageId = (int)$params['id'];
+        }
+
+        EidUtility::initTCA();
+    }
+
+    /**
+     * Generate link
+     *
+     * @return string
+     */
+    public function getLink()
+    {
+        if ($this->pageId) {
+            $this->createTSFE();
+
+            /** @var ContentObjectRenderer $cObj */
+            $cObj = GeneralUtility::makeInstance(ContentObjectRenderer::class);
+
+            $typoLinkConf = [
+                'parameter' => $this->pageId,
+                'forceAbsoluteUrl' => 1
+            ];
+
+            $url = $cObj->typoLink_URL($typoLinkConf) ?: '/';
+            $parts = parse_url($url);
+
+            return empty($parts['host']) ? GeneralUtility::locationHeaderUrl($url) : $url;
+        }
+
+        return '';
+    }
+
+    /**
+     * Initializes TSFE. This is necessary to have proper environment for typoLink.
+     *
+     * @return void
+     */
+    protected function createTSFE()
+    {
+        /** @var TypoScriptFrontendController $tsfe */
+        $tsfe = GeneralUtility::makeInstance(
+            TypoScriptFrontendController::class,
+            $GLOBALS['TYPO3_CONF_VARS'],
+            $this->pageId,
+            ''
+        );
+        $GLOBALS['TSFE'] = $tsfe;
+
+        $tsfe->connectToDB();
+        $tsfe->initFEuser();
+        $tsfe->determineId();
+        $tsfe->initTemplate();
+        $tsfe->getConfigArray();
+
+        // Set linkVars, absRefPrefix, etc
+        PageGenerator::pagegenInit();
+    }
+}
---
+++ ext_localconf.php
@@ -0,0 +1,16 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+// Register EID
+$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/Classes/Eid/LinkGeneratorEid.php';
+
+if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_pxasiteimprove_urls']['frontend'])) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_pxasiteimprove_urls'] = [
+        'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
+        'backend' => \TYPO3\CMS\Core\Cache\Backend\FileBackend::class,
+        'options' => [
+            'defaultLifetime' => 3600 * 24 // One day
+        ],
+        'groups' => ['all']
+    ];
+}
