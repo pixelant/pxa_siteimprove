03c67f061a920ac8e9fb2933b0cdcdf0d1d7e021
anjeylink <andriy.oprysko@resultify.se>
2018-08-23 11:49:32 +0300

    [TASK] fix PSR2

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,8 @@ defined('TYPO3_MODE') || die('Access denied.');
 // Register EID
 $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/Classes/Eid/LinkGeneratorEid.php';
 
-if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_pxasiteimprove_urls']['frontend'])) {
+// @codingStandardsIgnoreStart
+if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_pxasiteimprove_urls']['frontend'])) {// @codingStandardsIgnoreEnd
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_pxasiteimprove_urls'] = [
         'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
         'backend' => \TYPO3\CMS\Core\Cache\Backend\FileBackend::class,
