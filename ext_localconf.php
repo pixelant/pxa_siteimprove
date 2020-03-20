603b7691ce56a7cb2c203429fcbffbad08aa3462
mabolek <mathias@lilio.com>
2020-03-20 10:34:48 +0100

    [TASK] Replaced postUserLookUp with middleware

--- ext_localconf.php
+++ ext_localconf.php
@@ -17,8 +17,12 @@ if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][
 }
 
 if (\Pixelant\PxaSiteimprove\Utility\CompatibilityUtility::typo3VersionIsGreaterThanOrEqualTo(8000000)) {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp'][] =
-        \Pixelant\PxaSiteimprove\Hooks\DeepLinkingHandler::class . '->storeGoToRequestInUserSession';
+    if(\Pixelant\PxaSiteimprove\Utility\CompatibilityUtility::typo3VersionIsLessThan(9500000)) {
+        // Handled by middleware in >=9.5
+        // @extensionScannerIgnoreLine
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp'][] =
+            \Pixelant\PxaSiteimprove\Hooks\DeepLinkingHandler::class . '->storeGoToRequestInUserSession';
+    }
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/backend.php']['constructPostProcess'][]
         = \Pixelant\PxaSiteimprove\Hooks\DeepLinkingHandler::class . '->effectuateGoToRequest';
 }
