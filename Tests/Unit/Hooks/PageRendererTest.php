22468d2181ca49dbcea1089700ad7127a4faa435
mabolek <mathias@lilio.com>
2020-03-25 08:50:36 +0100

    [TASK] Removed unused aliases

--- Tests/Unit/Hooks/PageRendererTest.php
+++ Tests/Unit/Hooks/PageRendererTest.php
@@ -2,12 +2,7 @@
 
 namespace Pixelant\PxaSiteimprove\Tests\Unit\Hooks;
 
-use Pixelant\PxaSiteimprove\Hooks\PageRenderer;
-use TYPO3\CMS\Core\Page\PageRenderer as BackendPageRenderer;
-use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 use Nimut\TestingFramework\TestCase\UnitTestCase;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Lang\LanguageService;
 
 class PageRendererTest extends UnitTestCase
 {
