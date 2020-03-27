f03c0bb3fdeb395868f0790cbabb28be1ddcf38d
mabolek <mathias@lilio.com>
2020-03-27 12:58:07 +0100

    [WIP] Test testing

---
+++ Tests/Functional/Controller/AjaxBackendControllerTest.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace Pixelant\PxaSiteimprove\Tests\Functional\Controller;
+
+
+use Nimut\TestingFramework\TestCase\FunctionalTestCase;
+use Pixelant\PxaSiteimprove\Controller\AjaxBackendController;
+use TYPO3\CMS\Core\Http\ServerRequest;
+use TYPO3\CMS\Core\Core\Bootstrap;
+
+class AjaxBackendControllerTest extends FunctionalTestCase
+{
+    /**
+     * @var AjaxBackendController
+     */
+    protected $subject;
+
+    protected function setUp()
+    {
+        $this->configurationToUseInTestInstance['DB'] = [
+            'Connections' => [
+                'Default' => [
+                    'dbname'   => 'db',
+                    'host'     => 'localhost',
+                    'password' => 'root',
+                    'user'     => 'root',
+                    'port'     => '3306',
+                ]
+            ]
+        ];
+
+
+        parent::setUp();
+
+        $this->importDataSet('PACKAGE:nimut/testing-framework/res/Fixtures/Database/pages.xml');
+        $this->importDataSet('PACKAGE:nimut/testing-framework/res/Fixtures/Database/sys_language.xml');
+
+        $this->setUpBackendUserFromFixture(1);
+        $this->setUpFrontendRootPage(
+            1,
+            ['PACKAGE:nimut/testing-framework/res/Fixtures/TypoScript/JsonRenderer.ts'],
+            ['PACKAGE:nimut/testing-framework/res/Fixtures/Sites/Frontend/site.yaml']
+        );
+        Bootstrap::initializeLanguageObject();
+
+        $this->subject = new AjaxBackendController();
+    }
+
+    /**
+     * @test
+     */
+    public function getPageLinkActionReturnsCorrectUrl()
+    {
+        $request = new ServerRequest();
+        $request->withQueryParams(['id' => 1]);
+
+        $response = $this->subject->getPageLinkAction($request);
+        $body = (string)$response->getBody();
+        $jsonArray = json_decode($body, true);
+
+        var_dump($body);
+    }
+}
