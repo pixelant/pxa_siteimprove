5d7f2787bad08bcbd32dd034aad6937618210e5f
mabolek <mathias@lilio.com>
2020-03-30 20:26:59 +0200

    [WIP] Test testing

--- Classes/Utility/CompatibilityUtility.php
+++ Classes/Utility/CompatibilityUtility.php
@@ -29,9 +29,10 @@ class CompatibilityUtility
         if (self::typo3VersionIsGreaterThanOrEqualTo(9500000)) {
             /** @var SiteFinder $siteFinder */
             $siteFinder = GeneralUtility::makeInstance(SiteFinder::class);
-
+var_dump($siteFinder);
             try {
                 $site = $siteFinder->getSiteByPageId($pageId);
+                var_dump($site);
                 $pageLink = (string) $site->getRouter()->generateUri($pageId);
             } catch (SiteNotFoundException $siteNotFoundException) {
                 $pageLink = '';
