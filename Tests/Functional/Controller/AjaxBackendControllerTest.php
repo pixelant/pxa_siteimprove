443d900505dd2ec9ac7c76850382531b777d3534
mabolek <mathias@lilio.com>
2020-04-17 23:03:21 +0200

    [TASK] Add type hinting

--- Tests/Functional/Controller/AjaxBackendControllerTest.php
+++ Tests/Functional/Controller/AjaxBackendControllerTest.php
@@ -52,7 +52,7 @@ class AjaxBackendControllerTest extends FunctionalTestCase
     {
         $request = (new ServerRequest())->withQueryParams(['id' => 2]);
         $response = $this->subject->getPageLinkAction($request);
-        $body = $response->getBody()->getContents();
+        $body = (string)$response->getBody();
 
         if (CompatibilityUtility::typo3VersionIsGreaterThanOrEqualTo(10000000)) {
             $this->assertEquals(
