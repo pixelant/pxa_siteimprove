818282c18dd944e1412f5a576791cf0366182f93
mabolek <mathias@lilio.com>
2020-04-20 23:23:16 +0200

    [TASK] Created tests for DeepLinkingHandler

---
+++ Tests/Unit/Hooks/DeepLinkingHandlerTest.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Pixelant\PxaSiteimprove\Tests\Unit\Hooks;
+
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use Pixelant\PxaSiteimprove\Hooks\DeepLinkingHandler;
+use Pixelant\PxaSiteimprove\Utility\CompatibilityUtility;
+use TYPO3\CMS\Backend\Controller\BackendController;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+
+class DeepLinkingHandlerTest extends UnitTestCase
+{
+    /**
+     * @var DeepLinkingHandler
+     */
+    protected $subject;
+
+    /**
+     * Setup method
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->subject = new DeepLinkingHandler();
+    }
+
+    /**
+     * @test
+     */
+    public function storeGoToRequestInUserSessionCorrectly()
+    {
+        if ($this->typo3VersionSupportsDeeplinking()) {
+            $expected = 'page:1:2';
+
+            $backendUserAuthentication = $this->createMock(BackendUserAuthentication::class);
+            $backendUserAuthentication
+                ->expects($this->once())
+                ->method('setAndSaveSessionData')
+                ->with(
+                    $this->equalTo('tx_siteimprove_goto'),
+                    $this->equalTo($expected)
+                );
+
+            $_REQUEST['tx_siteimprove_goto'] = $expected;
+            $GLOBALS['BE_USER'] = [];
+
+            $this->subject->storeGoToRequestInUserSession(
+                [],
+                $backendUserAuthentication
+            );
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function storeGoToRequestInUserSessionSkipsWhenNoUserSet()
+    {
+        if ($this->typo3VersionSupportsDeeplinking()) {
+            $expected = 'page:1:2';
+
+            $backendUserAuthentication = $this->createMock(BackendUserAuthentication::class);
+            $backendUserAuthentication
+                ->expects($this->never())
+                ->method('setAndSaveSessionData')
+                ->with(
+                    $this->equalTo('tx_siteimprove_goto'),
+                    $this->equalTo($expected)
+                );
+
+            $_REQUEST['tx_siteimprove_goto'] = $expected;
+            $GLOBALS['BE_USER'] = null;
+
+            $this->subject->storeGoToRequestInUserSession(
+                [],
+                $backendUserAuthentication
+            );
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function storeGoToRequestInUserSessionSkipsWhenNoArgumentSet()
+    {
+        if ($this->typo3VersionSupportsDeeplinking()) {
+            $expected = 'page:1:2';
+
+            $backendUserAuthentication = $this->createMock(BackendUserAuthentication::class);
+            $backendUserAuthentication
+                ->expects($this->never())
+                ->method('setAndSaveSessionData')
+                ->with(
+                    $this->equalTo('tx_siteimprove_goto'),
+                    $this->equalTo($expected)
+                );
+
+            $GLOBALS['BE_USER'] = [];
+
+            $this->subject->storeGoToRequestInUserSession(
+                [],
+                $backendUserAuthentication
+            );
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function effectuateGoToRequestCallsPageTypeHandlerCorrectly()
+    {
+        if ($this->typo3VersionSupportsDeeplinking()) {
+            $expected = 'web_layout->id=1&SET[language]=2';
+
+
+            $backendUserAuthentication = $this->createMock(BackendUserAuthentication::class);
+
+            $backendUserAuthentication
+                ->expects($this->once())
+                ->method('getSessionData')
+                ->willReturn('page:1:2');
+
+            $backendController = $this->createMock(BackendController::class);
+
+            $GLOBALS['BE_USER'] = $backendUserAuthentication;
+            $GLOBALS['BE_USER']->uc = [];
+
+            $this->subject->effectuateGoToRequest(
+                [],
+                $backendController
+            );
+
+            $this->assertEquals(
+                $expected,
+                $GLOBALS['BE_USER']->uc['startModuleOnFirstLogin']
+            );
+        }
+    }
+
+    protected function typo3VersionSupportsDeeplinking()
+    {
+        if (CompatibilityUtility::typo3VersionIsLessThan('8.0')) {
+            $this->markTestSkipped('Not used in TYPO3 versions <8.0');
+            return false;
+        }
+
+        return true;
+    }
+
+}
